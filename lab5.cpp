dd1106bb52f313320d8fc35847910cdb1e224773
Danila <dankkk11@gmail.com>
2024-10-18 18:26:40 +0300

    Задание 2

--- lab5.cpp
+++ lab5.cpp
@@ -64,6 +64,56 @@ int main() {
         if (g == size - 1) std::cout << "Вершина " << i + 1 << " - доминирующая" << std::endl;
     }
 
+    std::cout << "\n=========================================================================================\n";
+
+    std::cout << "Матрица инцидентности:\n\n";
+    int** arr1 = new int* [size];
+    for (int i = 0; i < size; ++i) {
+        arr1[i] = new int[count_smez] {};
+    }
+
+    int d = 0;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (arr[i][j] == 1 && i < j) {
+                arr1[i][d] = 1;
+                arr1[j][d] = 1;
+                d++;
+            }
+        }
+    }
+
+    int count_in = 0;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < count_smez; ++j) {
+            std::cout << arr1[i][j] << "\t";
+            if (j + 1 > count_in) {
+                count_in = j + 1;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "\nКоличество рёбер (размер) графа через матрицу инцидентности: " << count_in << std::endl;
+
+    for (int i = 0; i < size; ++i) {
+        int g = 0;
+        for (int j = 0; j < count_smez; ++j) {
+            if (arr1[i][j] == 1) {
+                g++;
+            }
+        }
+
+        if (g == 0) std::cout << "Вершина " << i + 1 << " - изолированная" << std::endl;
+        if (g == 1) std::cout << "Вершина " << i + 1 << " - концевая" << std::endl;
+        if (g == size - 1) std::cout << "Вершина " << i + 1 << " - доминирующая" << std::endl;
+    }
+
+    for (int i = 0; i < size; ++i) {
+        delete[] arr1[i];
+    }
+    delete[] arr1;
+
     for (int i = 0; i < size; ++i) {
         delete[] arr[i];
     }
